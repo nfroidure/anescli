388d80611c6526e88e51951a606754977078e383
Nicolas Froidure <nicolas.froidure@insertafter.com>
2017-06-26 09:16:02 +0200

    fix(Config): Remove unused configs

--- config.example.js
+++ config.example.js
@@ -1,13 +1,4 @@
 module.exports = {
   project: 'myproject',
   elastic: 'https://es.example.com',
-  pg: {
-    user: 'postgres',
-    database: 'catalog',
-    password: 'xxxxxx',
-    host: 'localhost',
-    port: 5432,
-    max: 1,
-    idleTimeoutMillis: 30000,
-  },
 };
